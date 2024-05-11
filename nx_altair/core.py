fdce12e137aca61076444a1bc0c5d9082db03168
T-Flet <T-Fletcher@outlook.com>
2024-05-11 16:35:18 +0100

    Added curved edges, configurable with control points and interpolation method. They were/are a necessity for directed graphs, otherwise double edges cannot be seen.

--- nx_altair/core.py
+++ nx_altair/core.py
@@ -2,7 +2,7 @@ import pandas as pd
 import networkx as nx
 import altair as alt
 from ._utils import despine
-from math import atan2, sin, cos
+from math import atan2, sin, cos, sqrt
 
 
 def to_pandas_nodes(G, pos):
@@ -12,20 +12,43 @@ def to_pandas_nodes(G, pos):
 
 
 
-def to_pandas_edges(G, pos, **kwargs):
+def to_pandas_edges(G, pos, control_points = None, **kwargs):
     '''Convert Graph edges to pandas DataFrame that's readable to Altair.
+
+    control_points : list[tuple[float, float]]
+        Points to insert in the dataframe between the source and target point rows of each edge; they should be expressed in coordinates relative to their straight edge:
+        (proportion of edge length parallel to the edge, proportion of edge length perpendicular (anticlockwise) to the edge).
+        E.g. [(0.5, 0.1)] is a single control point halfway along the edge and 0.1 of its length to the left of it.
     '''
     rows = []
     for i, e in enumerate(G.edges):
+        Dx = pos[e[1]][0] - pos[e[0]][0]
+        Dy = pos[e[1]][1] - pos[e[0]][1]
+        D = sqrt(Dx ** 2 + Dy ** 2)
+        angle = atan2(Dy, Dx)
+        order = 0
+
         rows.append(dict(
-            edge = i,
+            edge = i, order = order,
             source = e[0], target = e[1], pair = e,
             x = pos[e[0]][0], y = pos[e[0]][1],
             **G.edges[e]
         ))
+        order += 1
+
+        if control_points is not None:
+            for v, w in control_points: # Pairs of relative coordinates: (D proportion parallel to D, D proportion perpendicular to D)
+                rows.append(dict(
+                    edge = i, order = order,
+                    source = e[0], target = e[1], pair = e,
+                    x = pos[e[0]][0] + D * (v * cos(angle) - w * sin(angle)),
+                    y = pos[e[0]][1] + D * (v * sin(angle) + w * cos(angle)),
+                    **G.edges[e]
+                ))
+                order += 1
 
         rows.append(dict(
-            edge = i,
+            edge = i, order = order,
             source = e[0], target = e[1], pair = e,
             x = pos[e[1]][0], y = pos[e[1]][1],
             **G.edges[e]
@@ -35,15 +58,28 @@ def to_pandas_edges(G, pos, **kwargs):
 
 
 
-def to_pandas_edges_arrows(G, pos, arrow_length, arrow_length_is_relative = False, **kwargs):
+def to_pandas_edges_arrows(G, pos, arrow_length, arrow_length_is_relative = False, control_points = None, **kwargs):
     '''Convert Graph edges to pandas DataFrame that's readable to Altair.
-    If arrow_length_is_relative, then arrow_length is interpreted as the proportion of the corresponding edge length.
+    If arrow_length_is_relative, then arrow_length is interpreted as the proportion of the corresponding edge length
+    (it is interpreted as a proportion of the full length even if the edge is curved and the arrow angle is therefore based on the last control point).
+    
+    control_points : list[tuple[float, float]]
+        Same argument as to_pandas_edges.
+        NOTE that there is no curved_edges argument; it is assumed that if control_points is not None then the edges are indeed curved.
+        Only the last control point is used for arrow purposes.
     '''
     rows = []
     for i, e in enumerate(G.edges):
         Dx = pos[e[1]][0] - pos[e[0]][0]
         Dy = pos[e[1]][1] - pos[e[0]][1]
+        D = sqrt(Dx ** 2 + Dy ** 2)
         angle = atan2(Dy, Dx)
+
+        if control_points is not None: # Shift direction from the source point to the last control point
+            v, w = control_points[-1]
+            Dx = pos[e[1]][0] - (pos[e[0]][0] + D * (v * cos(angle) - w * sin(angle)))
+            Dy = pos[e[1]][1] - (pos[e[0]][1] + D * (v * sin(angle) + w * cos(angle)))
+            angle = atan2(Dy, Dx)
 
         rows.append(dict(
             edge = i,
@@ -53,10 +89,10 @@ def to_pandas_edges_arrows(G, pos, arrow_length, arrow_length_is_relative = Fals
         ))
 
         rows.append(dict(
-            edge = i,
+            edge = i, # Yes, if arrow_length_is_relative Dx & Dy are reassembled from D and cos & sin; this is to keep the same expression regardless of control_points
             source = e[0], target = e[1], pair = e,
-            x = pos[e[1]][0] - arrow_length * (Dx if arrow_length_is_relative else cos(angle)),
-            y = pos[e[1]][1] - arrow_length * (Dy if arrow_length_is_relative else sin(angle)),
+            x = pos[e[1]][0] - arrow_length * (D if arrow_length_is_relative else 1) * cos(angle),
+            y = pos[e[1]][1] - arrow_length * (D if arrow_length_is_relative else 1) * sin(angle),
             **G.edges[e]
         ))
 
@@ -65,7 +101,7 @@ def to_pandas_edges_arrows(G, pos, arrow_length, arrow_length_is_relative = Fals
 
 
 def to_chart(G, pos):
-    '''Construct a single Altair Chart for
+    '''Construct a single Altair Chart for the given Graph and node positions.
     '''
     node_df = to_pandas_nodes(G, pos)
     node_layer = alt.Chart(node_df)
--- nx_altair/draw_altair.py
+++ nx_altair/draw_altair.py
@@ -10,6 +10,7 @@ from ._utils import is_arraylike
 def draw_networkx_edges(G = None, pos = None,
     chart = None, layer = None,
     edgelist = None, width = 1, alpha = 1.0, edge_color = 'black', edge_cmap = None,
+    curved_edges = False, control_points = None, interpolate = 'basis',
     tooltip = None, legend = False, **kwargs):
     '''Draw the edges of the graph G.
 
@@ -24,29 +25,52 @@ def draw_networkx_edges(G = None, pos = None,
        A dictionary with nodes as keys and positions as values.
        Positions should be sequences of length 2.
 
-    chart:
+    chart : altair chart
+        A chart to plot on instead of creating a new one
 
     edgelist : collection of edge tuples
-       Draw only specified edges(default = G.edges)
+       Draw only specified edges
 
     width : float, or array of floats
-       Line width of edges (default = 1.0)
+       Line width of edges
 
     edge_color : colour string or edge attribute name
        In the attribute case, its values need to be colour strings if edge_cmap is None and floats if not None.
 
     alpha : float
-       The edge transparency (default = 1.0)
+       The edge transparency
 
     edge_cmap : Matplotlib colormap
-       Colormap for mapping intensities of edges; SILENTLY IGNORED unless edge_color is the name of an edge attribute containing floats (default = None)
+       Colormap for mapping intensities of edges; SILENTLY IGNORED unless edge_color is the name of an edge attribute containing floats
+
+    curved_edges : bool
+        Whether edges should be curved (using control_points and interpolate arguments)
+    
+    control_points : list[tuple[float, float]]
+        The control points to use the interpolation method on; they should be expressed in coordinates relative to their straight edge:
+        (proportion of edge length parallel to the edge, proportion of edge length perpendicular (anticlockwise) to the edge).
+        E.g. the default value of [(0.5, 0.1)] is a single control point halfway along the edge and 0.1 of its length to the left of it.
+    
+    interpolate : str
+        Interpolation method for curved edges (which are built on control points provided in edge-relative coordinates in curved_edges).
+        The default interpolation is a cubic spline (i.e. 'basis').
+        Interactive examples of possible values: https://altair-viz.github.io/user_guide/marks/line.html
+        Corresponding descriptions: https://d3js.org/d3-shape/curve
+
+        Recommendation:
+        - 'basis', 'catmull-rom' and 'bundle' (and of course 'linear' and 'monotone') are best since they do not overshoot if control points are close to endpoints
+        - the open and closed interpolation varieties are not appropriate for edges
+        - the 'natural' cubic and 'cardinal' interpolations tend to overshoot for control points close to endpoints
 
     Returns
     -------
     viz: ``altair.Chart`` object
     '''
+    if curved_edges and control_points is None: control_points = [(0.5, 0.1)]
+    elif not curved_edges: control_points = None # Because to_pandas_edges relies only on it to know whether edges are supposed to be curved
+
     if chart is None:
-        df_edges = to_pandas_edges(G, pos)
+        df_edges = to_pandas_edges(G, pos, control_points = control_points)
         edge_chart = alt.Chart(df_edges)
     else:
         df_edges = chart.layer[0].data
@@ -82,6 +106,11 @@ def draw_networkx_edges(G = None, pos = None,
     elif isinstance(alpha, (int, float)): marker_attrs['opacity'] = alpha
     elif alpha is not None: raise TypeError('alpha must be a string or None.')
 
+    # Curved edges
+    if curved_edges:
+        if isinstance(interpolate, str): marker_attrs['interpolate'] = interpolate
+        else: raise TypeError('interpolate must be a string.')
+    
     # Tooltip
     if tooltip is not None: encoded_attrs['tooltip'] = tooltip
 
@@ -91,7 +120,7 @@ def draw_networkx_edges(G = None, pos = None,
     edge_chart = edge_chart.mark_line(**marker_attrs).encode(
         x = alt.X('x', axis = alt.Axis(title = '', grid = False, labels = False, ticks = False)),
         y = alt.Y('y', axis = alt.Axis(title = '', grid = False, labels = False, ticks = False)),
-        detail = 'edge', **encoded_attrs
+        detail = 'edge', order = 'order', **encoded_attrs
     )
 
     if chart is not None: chart.layer[0] = edge_chart
@@ -102,7 +131,8 @@ def draw_networkx_edges(G = None, pos = None,
 
 def draw_networkx_arrows(G = None, pos = None,
     chart = None, layer = None,
-    edgelist = None, arrow_width = 2, arrow_length = 0.05, arrow_length_is_relative = False, alpha = 1.0, edge_color = 'black', edge_cmap = None,
+    edgelist = None, arrow_width = 2, arrow_length = 0.1, arrow_length_is_relative = True, alpha = 1.0, edge_color = 'black', edge_cmap = None,
+    curved_edges = False, control_points = None,
     tooltip = None, legend = False, **kwargs):
     '''Draw the edges of the graph G.
 
@@ -117,35 +147,45 @@ def draw_networkx_arrows(G = None, pos = None,
        A dictionary with nodes as keys and positions as values.
        Positions should be sequences of length 2.
 
-    chart:
+    chart : altair chart
+        A chart to plot on instead of creating a new one
 
     edgelist : collection of edge tuples
-       Draw only specified edges(default = G.edges)
+       Draw only specified edges
 
-    arrow_width : float, optional (default = 2.0)
+    arrow_width : float, optional
        The width of arrow portions of edges.
 
-    arrow_length : float, optional (default = .1)
+    arrow_length : float, optional
        The proportion of the line to be occupied by the arrow.
     
-    arrow_length_is_relative: bool, optional (default = False)
+    arrow_length_is_relative: bool, optional
         Whether arrow_length should be interpreted as a proportion of its edge length instead of an absolute measure.
 
     edge_color : colour string or edge attribute name
        In the attribute case, its values need to be colour strings if edge_cmap is None and floats if not None.
 
     alpha : float
-       The edge transparency (default = 1.0)
+       The edge transparency
 
     edge_cmap : Matplotlib colormap
-       Colormap for mapping intensities of edges; SILENTLY IGNORED unless edge_color is the name of an edge attribute containing floats (default = None)
+       Colormap for mapping intensities of edges; SILENTLY IGNORED unless edge_color is the name of an edge attribute containing floats
+    
+    curved_edges : bool
+        Same argument as draw_networkx_edges: whether the edges to be arrow-ed are curved
+    
+    control_points : list[tuple[float, float]]
+        Same argument as to_pandas_edges; only the last control point is used for arrow purposes
 
     Returns
     -------
     viz: ``altair.Chart`` object
     '''
+    if curved_edges and control_points is None: control_points = [(0.5, 0.1)]
+    elif not curved_edges: control_points = None # Because to_pandas_edges_arrows relies only on it to know whether edges are supposed to be curved
+
     if chart is None:
-        df_edge_arrows = to_pandas_edges_arrows(G, pos, arrow_length, arrow_length_is_relative = arrow_length_is_relative)
+        df_edge_arrows = to_pandas_edges_arrows(G, pos, arrow_length, arrow_length_is_relative = arrow_length_is_relative, control_points = control_points)
         edge_chart = alt.Chart(df_edge_arrows)
     else:
         df_edge_arrows = chart.layer[0].data
@@ -217,14 +257,14 @@ def draw_networkx_nodes(G = None, pos = None,
        Positions should be sequences of length 2.
 
     nodelist : list, optional
-       Draw only specified nodes (default G.nodes)
+       Draw only specified nodes
 
     node_size : scalar or string
-       Size of nodes (default = 300).  If an array is specified it must be the
+       Size of nodes; if an array is specified it must be the
        same length as nodelist.
 
     node_color : color string, or array of floats
-       Node color. Can be a single color format string (default = 'r'),
+       Node color. Can be a single color format string,
        or a  sequence of colors with the same length as nodelist.
        If numeric values are specified they will be mapped to
        colors using the cmap and vmin,vmax parameters.  See
@@ -232,19 +272,19 @@ def draw_networkx_nodes(G = None, pos = None,
 
     node_shape : string
        The shape of the node.  Specification is as matplotlib.scatter
-       marker, one of 'so^>v<dph8' (default = 'o').
+       marker, one of 'so^>v<dph8'.
 
     linewidths : float, or array of floats
-       Line width for nodes (default = 1.0)
+       Line width for nodes
 
     alpha : float or array of floats
-       The node transparency.  This can be a single alpha value (default = 1.0),
+       The node transparency.  This can be a single alpha value,
        in which case it will be applied to all the nodes of color. Otherwise,
        if it is an array, the elements of alpha will be applied to the colors
        in order (cycling through alpha multiple times if necessary).
 
     cmap : Matplotlib colormap
-       Colormap for mapping intensities of nodes (default = None)
+       Colormap for mapping intensities of nodes
 
     Returns
     -------
@@ -328,14 +368,14 @@ def draw_networkx_labels(G = None, pos = None,
        Positions should be sequences of length 2.
 
     nodelist : list, optional
-       Draw only specified nodes (default G.nodes)
+       Draw only specified nodes
 
     font_size : scalar or string
-       Size of nodes (default = 15).  If an array is specified it must be the
+       Size of nodes.  If an array is specified it must be the
        same length as nodelist.
 
     font_color : color string, or array of floats
-       Node color. Can be a single color format string (default = 'r'),
+       Node color. Can be a single color format string,
        or a  sequence of colors with the same length as nodelist.
        If numeric values are specified they will be mapped to
        colors using the cmap and vmin,vmax parameters.  See
@@ -397,30 +437,31 @@ def draw_networkx(G = None, pos = None,
     node_size = 300, node_color = 'green',
     node_label = None, font_color = 'black', font_size = 15,
     alpha = 1, cmap = None, linewidths = 1.0, width = 1,
-    arrow_width = 2, arrow_length = 0.05, arrow_length_is_relative = False,
+    arrow_width = 2, arrow_length = 0.1, arrow_length_is_relative = True,
     edge_color = 'grey', arrow_color = 'black',
     node_tooltip = None, edge_tooltip = None,
-    edge_cmap = None, arrow_cmap = None):
+    edge_cmap = None, arrow_cmap = None,
+    curved_edges = False, control_points = None, interpolate = 'basis'):
     '''Draw the graph G using Altair.
 
-    nodelist : list, optional (default G.nodes)
+    nodelist : list, optional
        Draw only specified nodes
 
-    edgelist : list, optional (default = G.edges)
+    edgelist : list, optional
        Draw only specified edges
     
-    show_orphans : bool (default = False)
+    show_orphans : bool
         Whether to plot nodes with no edges
 
-    show_self_loops : bool (default = False)
+    show_self_loops : bool
         Whether to plot edges starting and ending on the same node;
         nodes with only self-loops will still be plotted (though edge-less) unless show_orphans is also False
         
-    node_size : scalar or array, optional (default = 300)
+    node_size : scalar or array, optional
        Size of nodes.  If an array is specified it must be the
        same length as nodelist.
 
-    node_color : color string, or array of floats, (default = 'r')
+    node_color : color string, or array of floats,
        Node color. Can be a single color format string,
        or a  sequence of colors with the same length as nodelist.
        If numeric values are specified they will be mapped to
@@ -428,14 +469,14 @@ def draw_networkx(G = None, pos = None,
        matplotlib.scatter for more details.
 
     linewidths : float, or array of floats
-       Line width for nodes (default = 1.0)
+       Line width for nodes
     
     font_size : scalar or string
-       Size of nodes (default = 15).  If an array is specified it must be the
+       Size of nodes.  If an array is specified it must be the
        same length as nodelist.
 
     font_color : color string, or array of floats
-       Node color. Can be a single color format string (default = 'r'),
+       Node color. Can be a single color format string,
        or a  sequence of colors with the same length as nodelist.
        If numeric values are specified they will be mapped to
        colors using the cmap and vmin,vmax parameters.  See
@@ -444,22 +485,22 @@ def draw_networkx(G = None, pos = None,
     node_label : string
        The name of the node attribute to treat as a label.
 
-    alpha : float, optional (default = 1.0)
+    alpha : float, optional
        The node and edge transparency
 
-    cmap : Matplotlib colormap, optional (default = None)
+    cmap : Matplotlib colormap, optional
        Colormap for mapping intensities of nodes
 
-    width : float, optional (default = 1.0)
+    width : float, optional
        Line width of edges
 
-    arrow_width : float, optional (default = 2.0)
+    arrow_width : float, optional
        The width of arrow portions of edges.
 
-    arrow_length : float, optional (default = .1)
+    arrow_length : float, optional
        The proportion of the line to be occupied by the arrow.
 
-    arrow_length_is_relative: bool, optional (default = False)
+    arrow_length_is_relative: bool, optional
         Whether arrow_length should be interpreted as a proportion of its edge length instead of an absolute measure.
 
     edge_color : colour string or edge attribute name
@@ -468,11 +509,30 @@ def draw_networkx(G = None, pos = None,
     arrow_color : colour string or edge attribute name
        In the attribute case, its values need to be colour strings if edge_cmap is None and floats if not None.
 
-    edge_cmap : Matplotlib colormap, optional (default = None)
-       Colormap for mapping intensities of edges; SILENTLY IGNORED unless edge_color is the name of an edge attribute containing floats (default = None)
+    edge_cmap : Matplotlib colormap, optional
+       Colormap for mapping intensities of edges; SILENTLY IGNORED unless edge_color is the name of an edge attribute containing floats
 
-    arrow_cmap : Matplotlib colormap, optional (default = None)
-       Colormap for mapping intensities of edges; SILENTLY IGNORED unless edge_color is the name of an edge attribute containing floats (default = None)
+    arrow_cmap : Matplotlib colormap, optional
+       Colormap for mapping intensities of edges; SILENTLY IGNORED unless edge_color is the name of an edge attribute containing floats
+
+    curved_edges : bool
+        Whether edges should be curved (using control_points and interpolate arguments)
+    
+    control_points : list[tuple[float, float]]
+        The control points to use the interpolation method on; they should be expressed in coordinates relative to their straight edge:
+        (proportion of edge length parallel to the edge, proportion of edge length perpendicular (anticlockwise) to the edge).
+        E.g. the default value of [(0.5, 0.1)] is a single control point halfway along the edge and 0.1 of its length to the left of it.
+    
+    interpolate : str
+        Interpolation method for curved edges (which are built on control points provided in edge-relative coordinates in curved_edges).
+        The default interpolation is a cubic spline (i.e. 'basis').
+        Interactive examples of possible values: https://altair-viz.github.io/user_guide/marks/line.html
+        Corresponding descriptions: https://d3js.org/d3-shape/curve
+        
+        Recommendation:
+        - 'basis', 'catmull-rom' and 'bundle' (and of course 'linear' and 'monotone') are best since they do not overshoot if control points are close to endpoints
+        - the open and closed interpolation varieties are not appropriate for edges
+        - the 'natural' cubic and 'cardinal' interpolations tend to overshoot for control points close to endpoints
     '''
     if not (show_self_loops and show_orphans):
         G = deepcopy(G)
@@ -486,6 +546,7 @@ def draw_networkx(G = None, pos = None,
         edges = draw_networkx_edges(G, pos,
             edgelist = edgelist,
             alpha = alpha, width = width, edge_color = edge_color, edge_cmap = edge_cmap,
+            curved_edges = curved_edges, control_points = control_points, interpolate = interpolate,
             tooltip = edge_tooltip)
 
         # Arrows
@@ -493,6 +554,7 @@ def draw_networkx(G = None, pos = None,
             arrows = draw_networkx_arrows(G, pos,
                 edgelist = edgelist,
                 alpha = alpha, arrow_width = arrow_width, arrow_length = arrow_length, arrow_length_is_relative = arrow_length_is_relative, edge_color = arrow_color, edge_cmap = arrow_cmap,
+                curved_edges = curved_edges, control_points = control_points,
                 tooltip = edge_tooltip)
 
     # Nodes
